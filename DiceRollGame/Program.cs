9d738179715ae1c0b6c4eea492b5fbf34b20672c
regeh <regeh@M3005>
2023-01-18 21:57:45 -0700

    Add project files.

---
+++ DiceRollGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceRollGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int[] blah = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                blah[i] = 1 + i;
+
+                Console.WriteLine(blah[i]);
+                Console.WriteLine(r.Next(1, 7));
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                int roll = r.Next(6);
+
+                blah[roll]++;
+            }
+        }
+    }
+}
